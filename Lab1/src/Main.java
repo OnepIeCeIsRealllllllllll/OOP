69cd37c08f66c06e892e93ba84dfa4895cbfc826
gam_gam <144825492+OnepIeCeIsRealllllllllll@users.noreply.github.com>
2024-02-22 21:25:48 +0700

    Add files via upload

---
+++ Lab1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+      
+        double x = 0.25, y;
+        y = 3*Math.pow(x, 3) + 4*Math.pow(x, 2) + 8;
+        System.out.println(y);
+        
+
+    }
+}
+  
